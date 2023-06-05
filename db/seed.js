d85e86d9e6c5fbff95c96f566ec33d6624304a00
AleksBulajic <aleksandarbulajic@Aleksandars-MacBook-Pro.local>
2023-06-05 09:19:45 -0400

    sudo codes

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,17 @@
 //---------------------------------------------------------------------------------------------
-
+// Import the necessary modules
 const fs = require('fs');
 const axios = require('axios');
 
+
+// Make a GET request to the specified URL
 axios.get('https://www.freetogame.com/api/games')
   .then((response) => {
+        // Retrieve the data from the response
     const games = response.data;
 
+
+        // Write the data to a file named 'games.json'
     fs.writeFile('data/games.json', JSON.stringify(games, null, 2), (err) => {
       if (err) {
         console.error('Error writing to file:', err);
@@ -28,16 +33,17 @@ import Game from '../models/gamesModels.js'
 import games from '../data/games.json' assert {type: 'json'}
 // games.map((game) => ({...game, favorited: false}));
 
-
+// Define a function to seed the data into the database
 const seedData = async () => {
-    
+      // Drop the existing database
   db.dropDatabase()
     
+  // Insert the games data into the database
   await Game.insertMany(games)
   db.close()
 
 }
-
+// Call the seedData function to start seeding the data
 seedData()
 
 //-----data imporeted to mongosh----------//
